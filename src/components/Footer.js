54a5f298715e205a10ec5b80b089faab2ebfe616
princeyadav283203@gmail.com <princeyadav283203@gmail.com>
2022-07-17 03:03:53 +0530

    FOOTER IMPLEMENTED

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,46 @@
 import React from 'react'
 import "./Footer.css"
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import {Grid, Slider} from "@mui/material"
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
 function Footer() {
   return (
     <div className='footer'>
-        <h1>I am the footer</h1>
+        <div className='footer__left'>
+            <img className='footer__albumLogo'
+            src='https://media.istockphoto.com/vectors/love-music-neon-sign-vector-id1090431366?s=612x612'
+            alt=''/>
+            <div className='footer__songInfo'>
+              <h4>Yeah!</h4>
+              <p>Usher</p>
+
+            </div>
+        </div>
+        <div className='footer__center'>
+            <ShuffleIcon className='footer__green'/>
+            <SkipPreviousIcon className='footer__icon'/>
+            <PlayCircleOutlineIcon fontSize='large' className='footer__icon'/>
+            <SkipNextIcon className='footer__icon'/>
+            <RepeatIcon className='footer__green'/>
+        </div>
+        <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs >
+            <Slider   />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   )
 }
